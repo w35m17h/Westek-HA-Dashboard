ea2167e1854085eb6a070ef5bdcb1b4f9d297adf
Will Smith <westek@icloud.com>
2026-04-13 11:44:43 -0500

    Initial Commit

---
+++ Westek HA Dashboard/Westek_HA_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Westek_HA_DashboardApp.swift
+//  Westek HA Dashboard
+//
+//  Created by William E Smith on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Westek_HA_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
